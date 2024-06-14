b9eb9749fd06680a9113d922056c3119ec078886
FedericoG2 <fg0170700@gmail.com>
2024-06-14 20:38:48 -0300

    Agregar archivos de proyecto.

---
+++ ModeloParcial/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace ModeloParcial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstResumen = new System.Windows.Forms.ListBox();
+            this.cmbEstado = new System.Windows.Forms.ComboBox();
+            this.cmbTareas = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnFinalizar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtDescripcion);
+            this.groupBox1.Controls.Add(this.txtTitulo);
+            this.groupBox1.Location = new System.Drawing.Point(12, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(292, 320);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nueva Tarea";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(121, 259);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(104, 30);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Create Task";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Description";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Title";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(6, 148);
+            this.txtDescripcion.MaxLength = 50;
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDescripcion.Size = new System.Drawing.Size(219, 95);
+            this.txtDescripcion.TabIndex = 3;
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(6, 78);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(219, 20);
+            this.txtTitulo.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstResumen);
+            this.groupBox2.Controls.Add(this.cmbEstado);
+            this.groupBox2.Controls.Add(this.cmbTareas);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.btnFinalizar);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(310, 30);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(311, 320);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mis tareas";
+            // 
+            // lstResumen
+            // 
+            this.lstResumen.FormattingEnabled = true;
+            this.lstResumen.Location = new System.Drawing.Point(7, 148);
+            this.lstResumen.Name = "lstResumen";
+            this.lstResumen.Size = new System.Drawing.Size(275, 95);
+            this.lstResumen.TabIndex = 14;
+            // 
+            // cmbEstado
+            // 
+            this.cmbEstado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEstado.FormattingEnabled = true;
+            this.cmbEstado.Location = new System.Drawing.Point(6, 78);
+            this.cmbEstado.Name = "cmbEstado";
+            this.cmbEstado.Size = new System.Drawing.Size(121, 21);
+            this.cmbEstado.TabIndex = 13;
+            this.cmbEstado.SelectedIndexChanged += new System.EventHandler(this.cmbEstado_SelectedIndexChanged);
+            // 
+            // cmbTareas
+            // 
+            this.cmbTareas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTareas.FormattingEnabled = true;
+            this.cmbTareas.Location = new System.Drawing.Point(161, 78);
+            this.cmbTareas.Name = "cmbTareas";
+            this.cmbTareas.Size = new System.Drawing.Size(121, 21);
+            this.cmbTareas.TabIndex = 12;
+            this.cmbTareas.SelectedIndexChanged += new System.EventHandler(this.cmbTareas_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(158, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Tasks";
+            // 
+            // btnFinalizar
+            // 
+            this.btnFinalizar.Location = new System.Drawing.Point(178, 259);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.Size = new System.Drawing.Size(104, 30);
+            this.btnFinalizar.TabIndex = 9;
+            this.btnFinalizar.Text = "End Task";
+            this.btnFinalizar.UseVisualStyleBackColor = true;
+            this.btnFinalizar.Click += new System.EventHandler(this.btnFinalizar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "State";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Button btnFinalizar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstResumen;
+        private System.Windows.Forms.ComboBox cmbEstado;
+        private System.Windows.Forms.ComboBox cmbTareas;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ModeloParcial/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModeloParcial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Creo una lista de diccionarios de forma global y le precargo alunos diccionarios.
+        List<Dictionary<string, object>> listaTareas = new List<Dictionary<string, object>>() {
+    new Dictionary<string, object> { { "Title", "Study" }, { "Description", "Study for the exam" }, { "State", "Active" } },
+    new Dictionary<string, object> { { "Title", "Gym" }, { "Description", "Go to the gym in the afternoon" }, { "State", "Active" } },
+    new Dictionary<string, object> { { "Title", "Walk" }, { "Description", "Walk with the dog in the park" }, { "State", "Active" } },
+    new Dictionary<string, object> { { "Title", "Take medicine" }, { "Description", "Take antibiotic" }, { "State", "Completed" } },
+    new Dictionary<string, object> { { "Title", "Cook" }, { "Description", "Cook lunch" }, { "State", "Completed" } },
+};
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Precargo el combo estado en el inicio del programa
+            cmbEstado.Items.Clear();
+            cmbEstado.Items.Add("Active");
+            cmbEstado.Items.Add("Completed");
+        }
+
+        private void cmbEstado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnFinalizar.Enabled = true;
+            //Estado seleccionado en el combo lo guardo en una variable
+            string estadoSeleccionado = cmbEstado.SelectedItem.ToString();
+
+            //Limpio el combo tarea.
+            cmbTareas.Items.Clear();
+
+            //Recorro la lista de tareas preguntando por las tareas que tenga el mismo estado que el 
+           //estado seleccionado en el combo.
+          //Cargo en el combo el titulo de las tareas que cumplen la condicion.
+            foreach (var item in listaTareas)
+            {
+                string estado = (string)item["State"];
+
+                if (estadoSeleccionado == estado)
+                {
+                    cmbTareas.Items.Add(item["Title"]);
+                }
+            }
+
+            //Bloqueo el boton de finalizar tarea si quiero filtrar por tareas ya finalizadas.
+            if(estadoSeleccionado == "Completed")
+            {
+                btnFinalizar.Enabled = false;
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            //Guardo en variables lo que me viene por formulario.
+            string titulo = txtTitulo.Text;
+            string descripcion = txtDescripcion.Text;
+
+
+            //Si el titulo no es nulo y la descripcion menor a 50 creo un diccionario con las
+           //variables y lo agrego a la lista de tareas.
+            if (!string.IsNullOrWhiteSpace(titulo) && descripcion.Length < 50)
+            {
+             
+                Dictionary<string, object> nuevaTarea = new Dictionary<string, object>
+        
+                {
+            { "Title", titulo },
+            { "Description", descripcion },
+            { "State", "Active" }
+        
+                };
+
+                listaTareas.Add(nuevaTarea);
+            }
+            else
+            {
+                //Manejo el error segun corresponda.
+                if (string.IsNullOrWhiteSpace(titulo))
+                {
+                    MessageBox.Show("The title cannot be empty.");
+                }
+                if (descripcion.Length >= 50)
+                {
+                    MessageBox.Show("The description must be less than 50 characters long.");
+                }
+            }
+            //Limpio las cajas de texto.
+            txtTitulo.Text = string.Empty;
+            txtDescripcion.Text = string.Empty;
+
+        }
+
+        private void cmbTareas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Limpio el resumen cada que elijo una nueva tarea.
+            lstResumen.Items.Clear();
+
+            //Titulo de la tarea seleccionada en el combo.
+            string tituloSeleccionado = cmbTareas.SelectedItem.ToString();
+
+           //Recorro la lista de tareas buscando la que tenga el titulo seleecionado y cargo el resumen.
+            foreach (var item in listaTareas)
+            {
+                string titulo = (string)item["Title"];
+
+                if (tituloSeleccionado == titulo)
+                {
+                    lstResumen.Items.Add($"Name: {item["Title"]}");
+                    lstResumen.Items.Add($"Description: {item["Description"]}");
+                    lstResumen.Items.Add($"State: {item["State"]}");
+                    break;
+
+                }
+            }
+
+        }
+
+        private void btnFinalizar_Click(object sender, EventArgs e)
+        {
+            // Verificar si hay un título seleccionado en el ComboBox
+            if (cmbTareas.SelectedItem != null)
+            {
+                // Título de tarea seleccionado en el combo
+                string tituloSeleccionado = cmbTareas.SelectedItem.ToString();
+
+                // Recorro la lista buscando el título y cambio el estado a Finalizada
+                foreach (var item in listaTareas)
+                {
+                    string titulo = (string)item["Title"];
+
+                    if (tituloSeleccionado == titulo)
+                    {
+                        item["State"] = "Completed";
+                        MessageBox.Show($"Task '{titulo}' completed");
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                // Manejar el caso donde no hay ningún título seleccionado
+                MessageBox.Show("Please select a task from the list.");
+            }
+
+
+
+
+            lstResumen.Items.Clear();
+            cmbEstado.Items.Clear();
+            cmbTareas.Items.Clear();
+            cmbEstado.Items.Add("Active");
+            cmbEstado.Items.Add("Completed");
+
+
+
+        }
+    }
+}
